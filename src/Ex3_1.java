78dc8e12cfc8cff9674c17e805ec55e6b1c73a74
Kittikun (Art) Tungsang <kittikun.tu@aware.local>
2016-01-13 16:35:42 +0700

    add new src

---
+++ src/Ex3_1.java
@@ -0,0 +1,13 @@
+//package chapter3;
+
+//public class Ex3_1 {
+
+    //public static void main(String[] args) {
+        //int array[] = {1, 2, 3, 4};
+        //int total = 0;
+        //for (int i = 0; i < array.length; i++) {
+            //total += array[i];
+        //}
+        //System.out.println("total = " + total);
+    //}
+//}
