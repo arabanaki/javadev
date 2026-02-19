63e2688fb44de2e9375c271240911dbe4cb70637
arabanaki <arabanaki@gmail.com>
2026-02-19 21:07:21 +0700

    Create Maven OpenJDK example in com.biz.app.app4 (#19)

---
+++ chatgpt/src/main/java/com/biz/app/app4/GreetingService.java
@@ -0,0 +1,7 @@
+package com.biz.app.app4;
+
+public class GreetingService {
+    public String createGreeting(String name) {
+        return "Hello, " + name + "! Welcome to com.biz.app.app4";
+    }
+}
